f1cd61d08faecf245706c682f09aaa4fd1a133b4
Geo Angelopoulos <georgios@trality.com>
2021-12-20 14:43:22 +0100

    change path

--- plugins/vault-plugin-secrets-mock/run_docker.sh
+++ plugins/vault-plugin-secrets-mock/run_docker.sh
@@ -2,7 +2,7 @@
 
 docker run -d --name notary -p 8200:8200 -v $(pwd)/vault/plugins:/plugins \
 	-e 'VAULT_ADDR=http://127.0.0.1:8200' -e 'VAULT_DEV_ROOT_TOKEN_ID=root' -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' --cap-add=IPC_LOCK \
-	$1 sh -c "vault server -dev -dev-root-token-id=root -dev-plugin-dir=./plugins"
+	$1 sh -c "vault server -dev -dev-root-token-id=root -dev-plugin-dir=/plugins"
 
 sleep 20 && docker logs notary
 
