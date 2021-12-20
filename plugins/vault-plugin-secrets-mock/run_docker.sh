9c982cc3bf067358b9ee3f180ee0209cf1f89038
Geo Angelopoulos <georgios@trality.com>
2021-12-20 14:05:29 +0100

    try dockerised approach

---
+++ plugins/vault-plugin-secrets-mock/run_docker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker run -d --name notary -p 8200:8200 -v $(pwd)/vault/plugins:/plugins \
+	-e 'VAULT_ADDR=http://127.0.0.1:8200' -e 'VAULT_DEV_ROOT_TOKEN_ID=root' -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' --cap-add=IPC_LOCK \
+	$1 sh -c "vault server -dev -dev-root-token-id=root -dev-plugin-dir=./plugins"
+
+sleep 20 && docker logs notary
+
+docker exec notary /bin/sh -c "apk add file && file /plugins/* && file /bin/vault"
+
+docker exec notary /bin/sh -c "vault secrets enable -path=mock-secrets vault-plugin-secrets-mock"
